e0de1182e74e04e7969c80c41bbc3886a33dc779
Ivan Degn <ivan@adaptagency.com>
2018-09-27 10:13:31 +0200

    IFSUB-433 Update reepay.js integration javascript

--- js/reepay.js
+++ js/reepay.js
@@ -1,23 +1,36 @@
 (function($) {
-    $(document).ready(function () {
-        reepay.configure(drupalSettings.reepay.reepayApi);
+  $(document).ready(function () {
+    reepay.configure(drupalSettings.reepay.reepayApi);
+  });
+  $('form.commerce-checkout-flow').on('submit', function (event) {
+    var form = this;
+    event.preventDefault();
+    $('.error', form).removeClass('error');
+    $('.error-message', form).remove();
+    reepay.token(form, function (err, token) {
+      console.log(err, token);
+      if (err) {
+        // These errors are related to a field.
+        var field_error_codes = [
+          'validation',
+          'invalid-parameter',
+          'invalid-request',
+          'api-error',
+        ];
+        if (field_error_codes.indexOf(err.code) > -1) {
+          $(err.fields).each(function(key, val) {
+            $('[data-reepay="' + val + '"]', form).addClass('error');
+            $('[data-reepay="' + val + '"]', form).parent().append('<span class="error-message">' + Drupal.t(err.message) + '</span>');
+          });
+        }
+        else {
+          // Generic errors not related to a field.
+          $(form).append('<span class="error-message">' + Drupal.t(err.message) + '</span>');
+        }
+      } else {
+        // At this point we have a valid reepay token which has been put in the form, so we can submit it.
+        form.submit();
+      }
     });
-    $('form.commerce-checkout-flow').on('submit', function (event) {
-        var form = this;
-        event.preventDefault();
-        $('.error', form).removeClass('error');
-        $('.error-message', form).remove();
-        reepay.token(form, function (err, token) {
-            if (err) {
-                $(err.fields).each(function(key, val) {
-                   $('[data-reepay="' + val + '"]', form).addClass('error');
-                   $('[data-reepay="' + val + '"]', form).parent().append('<span class="error-message">Please enter a valid value</span>')
-                });
-                // handle error using err.code and err.fields
-            } else {
-                // The reepay-token field has automaticly been filled with the token
-                form.submit();
-            }
-        });
-    });
+  });
 })(jQuery);
