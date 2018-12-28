d716f457092d01dab5087534f7480d7e4d76f09f
Jesper Bisgaard <jesper@adapt.dk>
2018-12-27 20:30:47 -0500

    Added invoice order item.

---
+++ src/Model/InvoiceOrderItem.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Drupal\commerce_payment_reepay\Model;
+
+class InvoiceOrderItem {
+
+  public $ordertext = '';
+
+  public $amount = 0;
+
+  public $vat = 0;
+
+  public $quantity = 1;
+
+  public $amount_incl_vat = TRUE;
+
+  public function __construct() {
+
+  }
+
+
+  /**
+   * @return string
+   */
+  public function getOrderText(): string {
+    return $this->ordertext;
+  }
+
+  /**
+   * @param string $orderText
+   *
+   * @return \Drupal\commerce_payment_reepay\Model\InvoiceOrderItem
+   */
+  public function setOrderText(string $orderText): self {
+    $this->ordertext = $orderText;
+
+    return $this;
+  }
+
+  /**
+   * @return int
+   */
+  public function getAmount(): int {
+    return $this->amount;
+  }
+
+  /**
+   * @param int $amount
+   *
+   * @return \Drupal\commerce_payment_reepay\Model\InvoiceOrderItem
+   */
+  public function setAmount(int $amount): self {
+    $this->amount = $amount;
+
+    return $this;
+  }
+
+  /**
+   * @return int
+   */
+  public function getVat(): int {
+    return $this->vat;
+  }
+
+  /**
+   * @param int $vat
+   *
+   * @return \Drupal\commerce_payment_reepay\Model\InvoiceOrderItem
+   */
+  public function setVat(int $vat): self {
+    $this->vat = $vat;
+
+    return $this;
+  }
+
+  /**
+   * @return int
+   */
+  public function getQuantity(): int {
+    return $this->quantity;
+  }
+
+  /**
+   * @param int $quantity
+   *
+   * @return \Drupal\commerce_payment_reepay\Model\InvoiceOrderItem
+   */
+  public function setQuantity(int $quantity): self {
+    $this->quantity = $quantity;
+
+    return $this;
+  }
+
+  /**
+   * @return bool
+   */
+  public function isAmountInclVat(): bool {
+    return $this->amount_incl_vat;
+  }
+
+  /**
+   * @param bool $amount_incl_vat
+   *
+   * @return \Drupal\commerce_payment_reepay\Model\InvoiceOrderItem
+   */
+  public function setAmountInclVat(bool $amount_incl_vat): self {
+    $this->amount_incl_vat = $amount_incl_vat;
+
+    return $this;
+  }
+
+}
